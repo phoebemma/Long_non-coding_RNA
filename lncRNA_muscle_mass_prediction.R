7c34bd13ba9c33f630b2aa131948abe84a770049
Chidimma Echebiri <chidimma.atukpawurgmail.com>
2023-09-12 09:58:56 +0200

    update

--- lncRNA_muscle_mass_prediction.R
+++ lncRNA_muscle_mass_prediction.R
@@ -4,11 +4,12 @@ source("./R/Trainome_functions.R")
 #This dataset cntains only pre-exercise samples and the percentage change in muscle mass post exercise
 meta_df <- readr::read_csv("./data/all_metadata_with_pct_change.csv") %>%
   dplyr::select("participant", "sample_id", "study",
-                "sex", "age", "condition", "pct_change")%>%
+                "sex", "age", "condition", "pct_change", "mm_rank")%>%
   mutate(sex = factor(sex, levels = c("male", "female"))) %>%
   
   print()
 
+meta_df$sample_id[!duplicated(meta_df$sample_id), ]
 
 #Load the lncRNA data
 lncRNA  <- readr::read_csv("./data/lncRNAs.csv")
@@ -32,11 +33,16 @@ meta_df <- meta_df %>%
 #How many samples of each study are represented in the data
 table(meta_df$study)
 table(meta_df$sex)
-#table(meta_df$time)
+table(meta_df$time)
 
-hist(meta_df$pct_change)
+hist(meta_df$pct_change, col = "blue", border = "white", ylab = "Number of samples",
+     main = "Distribution of percentage change across samples",
+     xlab = "Percentage change across samples")
+hist(meta_df$mm_rank, col = "blue", border = "white", ylab = "Number of samples",
+     main = "Distribution of ranks across samples",
+     xlab = "Ranks across samples (0-1)")
 
-args<- list(formula = y ~  age  + sex  + age:pct_change + pct_change +(1|participant),
+args<- list(formula = y ~  age  + sex  + age:pct_change + pct_change + mm_rank + age:mm_rank +(1|participant),
             family = glmmTMB::nbinom2())
 ncores <- parallel::detectCores()
 results <- seq_wrapper(fitting_fun = glmmTMB::glmmTMB,
@@ -51,17 +57,17 @@ results <- seq_wrapper(fitting_fun = glmmTMB::glmmTMB,
                        subset = NULL,
                        cores = ncores)
 
-#saveRDS(results, file = "./data/pct_change_model.RDS")
+#saveRDS(results, file = "./data/muscle_mass_model.RDS")
 
 bind_rows(results$model_summarises) %>%
-  mutate(target = rep(names(results$model_summarises), each = 5)) %>%
+  mutate(target = rep(names(results$model_summarises), each = 7)) %>%
   filter(coef == "age") %>%
   ggplot(aes(Pr...z..)) + geom_histogram(bins = 80) +
   ggtitle("baseline lncRNA Pvalues age in numbers")
 
 
 bind_rows(results$model_summarises) %>%
-  mutate(target = rep(names(results$model_summarises), each = 5)) %>%
+  mutate(target = rep(names(results$model_summarises), each = 7)) %>%
   filter(coef == "pct_change") %>%
   ggplot(aes(Pr...z..)) + geom_histogram(bins = 80) +
   ggtitle("baseline lncRNA Pvalues for pct_change")
@@ -69,8 +75,22 @@ bind_rows(results$model_summarises) %>%
 
 
 bind_rows(results$model_summarises) %>%
-  mutate(target = rep(names(results$model_summarises), each = 5)) %>%
+  mutate(target = rep(names(results$model_summarises), each = 7)) %>%
   filter(coef == "age:pct_change") %>%
   ggplot(aes(Pr...z..)) + geom_histogram(bins = 80) +
   ggtitle("baseline lncRNA Pvalues age on pct_change")
 
+
+bind_rows(results$model_summarises) %>%
+  mutate(target = rep(names(results$model_summarises), each = 7)) %>%
+  filter(coef == "age:mm_rank") %>%
+  ggplot(aes(Pr...z..)) + geom_histogram(bins = 80) +
+  ggtitle("baseline lncRNA Pvalues age on muscle mass rank")
+
+
+
+bind_rows(results$model_summarises) %>%
+  mutate(target = rep(names(results$model_summarises), each = 7)) %>%
+  filter(coef == "mm_rank") %>%
+  ggplot(aes(Pr...z..)) + geom_histogram(bins = 80) +
+  ggtitle("baseline lncRNA Pvalues muscle mass rank")
